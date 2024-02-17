63f177405ee47b6ca91649a96096fcc067896c3d
Pavel Novikov <spl.novikov@gmail.com>
2024-02-17 15:41:17 +0300

    adds service and controller to create and get users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import * as process from 'process';
+
+import { UsersModule } from './users/users.module';
+import { User } from './users/users.model';
 
 @Module({
   controllers: [],
@@ -18,7 +20,7 @@ import * as process from 'process';
       username: process.env.POSTGRES_USERNAME,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
-      models: [],
+      models: [User],
       autoLoadModels: true,
     }),
     UsersModule,
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+
+import { User } from './users.model';
+import { CreateUserDTO } from './dto/create.user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User) private userRepository: typeof User) {}
+  async createUser(dto: CreateUserDTO) {
+    return await this.userRepository.create(dto);
+  }
+
+  async getAllUsers() {
+    return await this.userRepository.findAll();
+  }
+}
